b8d6bfd05d80408f4eeb9ce3d0aa3530a63aedf2
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 05:29:28 +0100

    commit zero

---
+++ Murder Mystery 2 Script pastebin roblox Murder Mystery 2 .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+MM2 script.Very nice and functional. It has functions: Murder ESP and Sherif ESP, Fly, setspeed and
+]]
