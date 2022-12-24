6f7b9dede150d058e2b0bf0850345da63e6427b7
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:23 +0100

    commit zero

---
+++ Murder Mystery 2 Script pastebin roblox Murder Mystery 2 Coin .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/72Z36Xe6'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Auto Farm with Anti-AFK
+						
+]]
