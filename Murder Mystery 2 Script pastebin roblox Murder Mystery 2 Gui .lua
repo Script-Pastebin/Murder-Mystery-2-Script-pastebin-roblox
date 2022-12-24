2d9175c077a6c7e973a78011f1a043c22cd0123d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:51:58 +0100

    commit zero

---
+++ Murder Mystery 2 Script pastebin roblox Murder Mystery 2 Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet('https://pastebin.com/raw/NwbndAwB'))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Auto-farm coin, Tp to gun (Doesn't work). Esp, and you know the roles of each player
+						
+]]
